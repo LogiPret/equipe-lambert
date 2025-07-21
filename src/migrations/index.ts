6b39a1e532fea3e57cb1a82c77802780b9f5a3b1
HugoOtth <hugo.otth@gmail.com>
2025-07-21 12:15:11 -0400

    Fix migration index file - remove reference to deleted migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,12 +1,6 @@
-import * as migration_20250714_175425_initial from './20250714_175425_initial';
 import * as migration_20250721_160245 from './20250721_160245';
 
 export const migrations = [
-  {
-    up: migration_20250714_175425_initial.up,
-    down: migration_20250714_175425_initial.down,
-    name: '20250714_175425_initial',
-  },
   {
     up: migration_20250721_160245.up,
     down: migration_20250721_160245.down,
